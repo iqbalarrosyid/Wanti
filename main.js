838d7c1c22d35f04f0b011932476f99606f25621
Iqbal Arrosyid <iqbalarrosyid551@gmail.com>
2024-09-27 19:37:53 +0700

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () =>{
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-        const text = "Wanti Nur Khasanah, Bagaikan bunga yang mekar dengan keindahan sempurna, kau hadir dengan ketulusan yang tak tergantikan. Seperti kelopak yang selalu terbuka, kau membentangkan hatimu untuk peduli pada semua orang, menjadi tempat berteduh bagi jiwa-jiwa yang lelah. Kebaikanmu mekar setiap hari, memberikan kehangatan tanpa pamrih, lebih peduli pada orang lain daripada dirimu sendiri. Kau adalah wanita yang tak pernah layu, tetap harum dan penuh cinta, selamanya menginspirasi.";
+        const text = "Wanti Nur Khasanah, Bagaikan bunga yang mekar dengan keindahan sempurna, kau hadir dengan ketulusan yang tak tergantikan. Seperti kelopak yang selalu terbuka, kau membentangkan hatimu untuk peduli pada semua orang, menjadi tempat berteduh bagi jiwa-jiwa yang lelah. Kebaikanmu mekar setiap hari, memberikan kehangatan tanpa pamrih, lebih peduli pada orang lain daripada dirimu sendiri. Kau adalah wanita yang tak pernah layu, tetap harum dan penuh cinta, selalu abadi, selamanya menginspirasi.";
         const speed = 60; // kecepatan ketikan dalam milidetik
         let i = 0;
       
